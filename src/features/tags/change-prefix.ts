c2c9d710eca73c9397325d27522bd4e35e8e841e
Ali Hammoud <letsmailali.h@gmail.com>
2026-05-08 05:15:25 +0300

    only allow staff to change prefix

--- src/features/tags/change-prefix.ts
+++ src/features/tags/change-prefix.ts
@@ -1,10 +1,41 @@
 import { prisma } from "@/db/prisma.js";
+import { env } from "@/env.js";
 import { setTagPrefix } from "@/util/tag-prefix.js";
-import { ChatInputCommandInteraction, MessageFlags } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  GuildMember,
+  MessageFlags,
+} from "discord.js";
 
 export const changeTagsPrefix = async (
   interaction: ChatInputCommandInteraction,
 ) => {
+  const commandUser = interaction.member;
+  if (!(commandUser instanceof GuildMember)) {
+    await interaction.reply({
+      content: "An error occurred while verifying your permissions.",
+      flags: MessageFlags.Ephemeral,
+    });
+    return;
+  }
+
+  const moderatorRole = await commandUser.guild.roles.fetch(
+    env.roles.moderator,
+  );
+
+  const isServerOwner = commandUser.guild.ownerId === commandUser.id;
+  const isModerator =
+    moderatorRole !== null &&
+    commandUser.roles.highest.position >= moderatorRole.position;
+
+  if (!isServerOwner && !isModerator) {
+    await interaction.reply({
+      content: "Only server owners and moderators can change the tag prefix.",
+      flags: MessageFlags.Ephemeral,
+    });
+    return;
+  }
+
   const newPrefix = interaction.options.getString("prefix", true);
 
   try {
