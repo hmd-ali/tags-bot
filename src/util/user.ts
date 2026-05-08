3f2e975e156bb450b8714e2e1ee82747dbd2ecf3
Ali Hammoud <letsmailali.h@gmail.com>
2026-05-08 20:28:26 +0300

    feat: add user utils

---
+++ src/util/user.ts
@@ -0,0 +1,27 @@
+import { env } from "@/env.js";
+import { ChatInputCommandInteraction, GuildMember } from "discord.js";
+
+export const getCommandUser = (
+  interaction: ChatInputCommandInteraction,
+): GuildMember | null => {
+  const commandUser = interaction.member;
+  if (!(commandUser instanceof GuildMember)) {
+    return null;
+  }
+  return commandUser;
+};
+
+export const isServerOwner = (member: GuildMember): boolean => {
+  return member.guild.ownerId === member.id;
+};
+
+export const isModerator = async (member: GuildMember) => {
+  const moderatorRole =
+    member.guild.roles.cache.get(env.roles.moderator) ||
+    (await member.guild.roles.fetch(env.roles.moderator));
+
+  return (
+    moderatorRole !== null &&
+    member.roles.highest.position >= moderatorRole.position
+  );
+};
