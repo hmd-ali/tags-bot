3be33b5fd7fcdb7c811a7205bf9f5884faab8a97
Ali Hammoud <letsmailali.h@gmail.com>
2026-05-09 04:08:04 +0300

    feat: add TagManager

--- src/features/tags/delete-tag.ts
+++ src/features/tags/delete-tag.ts
@@ -1,50 +1,31 @@
-import {
-  ChatInputCommandInteraction,
-  GuildMember,
-  MessageFlags,
-} from "discord.js";
+import { ChatInputCommandInteraction, MessageFlags } from "discord.js";
 import { prisma } from "@/db/prisma.js";
 import { TagsCache } from "@/cache/tags.js";
-import { env } from "@/env.js";
+import { getCommandUser, isModerator, isServerOwner } from "@/util/user.js";
+import { TagsManager } from "./tag.js";
+import { ErrorMessages } from "@/error-messages/index.js";
 
 export const deleteTagCommandHandler = async (
   interaction: ChatInputCommandInteraction,
 ) => {
   const name = interaction.options.getString("name", true);
-  const commandUser = interaction.member;
-  if (!(commandUser instanceof GuildMember)) {
-    await interaction.reply({
-      content: "An error occurred while verifying your permissions.",
-      flags: MessageFlags.Ephemeral,
-    });
+
+  const commandUser = getCommandUser(interaction);
+  if (commandUser === null) {
+    await interaction.reply(ErrorMessages.User.UnableToVerifyPermissions);
     return;
   }
-  const moderatorRole = await commandUser.guild.roles.fetch(
-    env.roles.moderator,
-  );
-  const isServerOwner = commandUser.guild.ownerId === commandUser.id;
-  const isModerator =
-    moderatorRole !== null &&
-    commandUser.roles.highest.position >= moderatorRole.position;
-
-  const tag =
-    TagsCache.get(name) || (await prisma.tag.findUnique({ where: { name } }));
-
+  const tag = await TagsManager.get(name);
   if (tag === null) {
-    await interaction.reply({
-      content: `Tag \`${name}\` not found.`,
-      flags: MessageFlags.Ephemeral,
-    });
+    await interaction.reply(ErrorMessages.Tags.TagNotFound(name));
     return;
   }
 
   const isTagOwner = tag.userId === commandUser.id;
+  const isUserModerator = await isModerator(commandUser);
 
-  if (!isServerOwner && !isModerator && !isTagOwner) {
-    await interaction.reply({
-      content: `You do not own the tag \`${name}\`. Only the owner and moderators can edit the tag.`,
-      flags: MessageFlags.Ephemeral,
-    });
+  if (!isServerOwner(commandUser) && !isUserModerator && !isTagOwner) {
+    await interaction.reply(ErrorMessages.Tags.OwnershipRequired);
     return;
   }
 
