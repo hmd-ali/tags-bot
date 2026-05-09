a58cf3cf1c612540dae060a4ca5c2a26cb6791c0
Ali Hammoud <letsmailali.h@gmail.com>
2026-05-09 04:07:11 +0300

    feat: export error messages

---
+++ src/error-messages/user.ts
@@ -0,0 +1,22 @@
+import {
+  Colors,
+  ContainerBuilder,
+  InteractionReplyOptions,
+  MessageFlags,
+  TextDisplayBuilder,
+} from "discord.js";
+
+export const User = {
+  UnableToVerifyPermissions: {
+    components: [
+      new ContainerBuilder()
+        .setAccentColor(Colors.Red)
+        .addTextDisplayComponents(
+          new TextDisplayBuilder().setContent(
+            "An error occurred while verifying your permissions.",
+          ),
+        ),
+    ],
+    flags: MessageFlags.Ephemeral | MessageFlags.IsComponentsV2,
+  } satisfies InteractionReplyOptions,
+};
