41c21a16418041e379a4acbd47cce390572aedfd
Ali Hammoud <letsmailali.h@gmail.com>
2026-05-11 07:16:27 +0300

    feat: add InvalidTagName error message

--- src/error-messages/tags.ts
+++ src/error-messages/tags.ts
@@ -14,4 +14,7 @@ export const Tags = {
 	OwnershipRequired: basicErrorMessage(
 		"You must be the owner of this tag to use this command."
 	),
+	InvalidTagName: basicErrorMessage(
+		"Tag names must be 1–32 characters, alphanumeric with hyphens/underscores, and cannot be purely numeric."
+	),
 } satisfies Record<string, ErrorMessage>;
