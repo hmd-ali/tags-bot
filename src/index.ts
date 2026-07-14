33e45958c851bc02eb9fc6aa218a257329e0c3ba
Ali Hammoud <letsmailali.h@gmail.com>
2026-07-15 02:32:27 +0300

    feat: add lint and formatter configs and scripts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { Client, GatewayIntentBits, Partials } from "discord.js";
-import { loadEvents } from "@/common/events/load-events.js";
-import { prisma } from "@/db/prisma.js";
-import { env } from "@/env.js";
-import { initBotOptions } from "./options.js";
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
+import { loadEvents } from '@/common/events/load-events.js';
+import { prisma } from '@/db/prisma.js';
+import { env } from '@/env.js';
+import { initBotOptions } from './options.js';
 
 const client = new Client({
-	intents:
-		GatewayIntentBits.Guilds |
-		GatewayIntentBits.GuildMessages |
-		GatewayIntentBits.GuildMessageReactions |
-		GatewayIntentBits.MessageContent,
-	partials: [
-		Partials.GuildMember,
-		Partials.Reaction,
-		Partials.Message,
-		Partials.User,
-	],
+  intents:
+    GatewayIntentBits.Guilds |
+    GatewayIntentBits.GuildMessages |
+    GatewayIntentBits.GuildMessageReactions |
+    GatewayIntentBits.MessageContent,
+  partials: [
+    Partials.GuildMember,
+    Partials.Reaction,
+    Partials.Message,
+    Partials.User,
+  ],
 });
 
 loadEvents(client);
@@ -23,12 +23,12 @@ loadEvents(client);
 client.login(env.discord.token);
 await initBotOptions();
 
-process.on("SIGINT", async () => {
-	await prisma.$disconnect();
-	process.exit(0);
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
 
-process.on("SIGTERM", async () => {
-	await prisma.$disconnect();
-	process.exit(0);
+process.on('SIGTERM', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
